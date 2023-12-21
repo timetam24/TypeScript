6d151e212f5f0c22a795d93c55f90fd773efee04
timetam <timetam24@naver.com>
2023-12-21 16:49:32 +0900

    첫 TypeScript 파일 만들기

---
+++ greeter.ts
@@ -0,0 +1,25 @@
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Time", "Tam", "User");
+
+document.body.textContent = greeter(user);
+
+// TypeScript 컴파일러 실행 명령: tsc 파일명
