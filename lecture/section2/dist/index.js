063e8e1cab49efd260c3baccb78a93fddadbcc59
timetam <timetam24@naver.com>
2023-12-28 17:09:23 +0900

    [Section 2] 원시타입과 리터럴타입

--- lecture/section2/dist/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello New Section");
-export {};
---
+++ lecture/section2/src/chapter1.ts
@@ -0,0 +1,26 @@
+// 원시타입과 리터럴타입
+// number
+let num1: number = 123;
+let num2: number = Infinity;
+let num3: number = NaN;
+
+// string
+let str1: string = "hi";
+let str2: string = "hi";
+let str3: string = `hi`;
+let str4: string = `hi ${num1}`;
+
+// boolean
+let bol1: boolean = true;
+let bol2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// 리터럴(값) 타입
+let numA: 10 = 10; //이제 numA에는 10이외의 값은 저장 불가
+let strA: "hello" = "hello";
+let boolA: true = true;
