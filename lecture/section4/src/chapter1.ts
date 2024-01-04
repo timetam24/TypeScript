c70217e75d0296189e66f47373d8588e5f25b81b
timetam <timetam24@naver.com>
2024-01-04 16:18:00 +0900

    [Section 4] 함수 타입 표현식과 호출 시그니쳐

---
+++ lecture/section4/src/chapter1.ts
@@ -0,0 +1,20 @@
+/*
+ * 함수 타입 표현식
+ */
+type Operation = (a: number, b: number) => number;
+
+const add: Operation = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const multiply: Operation = (a, b) => a * b;
+const divide: (a: number, b: number) => number = (a, b) => a / b; // 반드시 type 별칭과 함께 사용해야 한다? X
+
+/*
+ * 호출 시그니쳐
+ * (콜 시그니쳐)
+ */
+type Operation2 = { (a: number, b: number): number };
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multiply2: Operation2 = (a, b) => a * b;
+const divide2: Operation2 = (a, b) => a / b;
