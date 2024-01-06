051a7b8b299b2937e7a5db092b4ec3b120de498b
timetam <timetam24@naver.com>
2024-01-06 18:47:57 +0900

    [Section 7] 프로미스와 제네릭

---
+++ lecture/section7/src/chapter5.ts
@@ -0,0 +1,46 @@
+/*
+ * 프로미스
+ */
+
+const promise = new Promise<number>((resolve, reject) => {
+  setTimeout(() => {
+    resolve(20);
+    reject("~때문에 실패");
+  }, 3000);
+});
+
+promise.then(
+  (response) => console.log(response * 20) // 400
+);
+
+promise.catch((error) => {
+  if (typeof error === "string") {
+    console.log(error);
+  }
+});
+
+/*
+ * 프로미스를 반환하는 함수의 타입을 정의
+ */
+
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+}
+
+function fetchPost(): Promise<Post> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        title: "게시글 제목",
+        content: "게시글 내용",
+      });
+    }, 3000);
+  });
+}
+
+const postRequest = fetchPost();
+
+postRequest.then((post) => console.log(post.id));
