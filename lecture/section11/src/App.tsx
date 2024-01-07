315cce0c20058612b411fccf736fcbcd25439cc0
timetam <timetam24@naver.com>
2024-01-07 23:25:32 +0900

    [Section 11] 상태관리와 Props 2: useState에서 useReducer로 바꾸기

--- lecture/section11/src/App.tsx
+++ lecture/section11/src/App.tsx
@@ -1,19 +1,46 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect, useReducer } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import { Todo } from "./types";
 import TodoItem from "./components/TodoItem";
 
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | { type: "DELETE"; id: number };
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [...state, action.data];
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.id);
+    }
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([...todos, { id: idRef.current++, content: text }]);
+    dispatch({
+      type: "CREATE",
+      data: { id: idRef.current++, content: text },
+    });
   };
 
   const onClickDelete = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: "DELETE",
+      id: id,
+    });
   };
 
   useEffect(() => {
