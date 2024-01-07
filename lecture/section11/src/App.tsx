fb3968adea50a9c02b0b27179f0f01f0dcfee50f
timetam <timetam24@naver.com>
2024-01-07 22:52:31 +0900

    [Section 11] 상태관리와 Props 1

--- lecture/section11/src/App.tsx
+++ lecture/section11/src/App.tsx
@@ -1,7 +1,30 @@
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
+
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([...todos, { id: idRef.current++, content: text }]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd}></Editor>
+    </div>
+  );
 }
 
 export default App;
